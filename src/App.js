c5d599721612c3a72292d6bf265e484093e8fbcc
lauradurandv <26388264+lauradurandv@users.noreply.github.com>
2024-05-21 20:59:08 -0400

    practice styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import * as React from 'react';
 import axios
  from 'axios';
 import { type } from '@testing-library/user-event/dist/type';
+//Styling to fall back on
+import './index.css';
+//Main styling
+//import styles from './App.module.css';
+//for component styling
+import styled from 'styled-components';
+import {ReactComponent as Check} from './check.svg';
+
+
+
 //Global Variables
 
 
@@ -9,6 +19,83 @@ import { type } from '@testing-library/user-event/dist/type';
 const getTitle = (title) => (title);
 const API_Endpoint = 'https://hn.algolia.com/api/v1/search?query=';
 
+const StyledContainer = styled.div`
+  height:100vw;
+  padding: 20px;
+
+  background:#83a4d4;
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+
+  color: #171212;
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weight: 300;
+  letter-spacing: 2px;
+`;
+
+const StyledItem = styled.li`
+  display: flex;
+  align-item: center;
+  padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  a {
+    color: inherit;
+  }
+
+  width: ${(props) => props.width};
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+
+  transition: all 0.1s ease-in;
+
+  &:hover {
+    background: #171212;
+    color: #ffffff;
+  }
+`;
+const StyledButtonSmall = styled(StyledButton)`
+  padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20px 0;
+  display: flex;
+  align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-top: 1px solid #171212;
+  padding-left: 5px;
+  font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  border-bottom: 1px solid #171212;
+  background-color: transparent;
+  fonts-size: 24px;
+`;
+
+ 
 //Reducer
 const storiesReducer = (state, action) => {
   switch(action.type){
@@ -53,25 +140,28 @@ const List = ({list, onRemoveItem}) => (
 );
 
 const Item = ({item, onRemoveItem}) => (
-  <li>
-    <span>
+  <StyledItem>
+    <StyledColumn style={{ width: '40%'}}>
         <a href={item.url}>{item.title}</a>
-    </span>
-    <span>
+    </StyledColumn>
+    <StyledColumn style={{ width: '30%'}}>
       {item.author}
-    </span>
-    <span>
+    </StyledColumn>
+    <StyledColumn style={{ width: '10%'}}>
       {item.num_comments}
-    </span>
-    <span>
+    </StyledColumn>
+    <StyledColumn style={{ width: '10%'}}>
       {item.points}
+    </StyledColumn>
+    <span style={{ width: '10%'}}>
+      <StyledButtonSmall 
+        type="button" 
+        onClick={() => onRemoveItem(item)}
+      >
+        <Check height="18px" width="18px" />
+      </StyledButtonSmall>
     </span>
-    <span>
-      <button type="button" onClick={() => onRemoveItem(item)}>
-        Dismiss
-      </button>
-    </span>
-  </li>
+  </StyledItem>
 );
 
 const InputWithLabel = ({ id, value, type='text', onInputChange, isFocused, children }) => {
@@ -86,8 +176,8 @@ const InputWithLabel = ({ id, value, type='text', onInputChange, isFocused, chil
 
   return(
     <>
-      <label htmlFor={id}>{children}</label>
-      <input
+      <StyledLabel htmlFor={id}>{children}</StyledLabel>
+      <StyledInput
         ref={inputRef}
         id={id}
         type={type}
@@ -103,7 +193,7 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit,
 }) => (
-  <form onSubmit={onSearchSubmit}>
+  <StyledSearchForm onSubmit={onSearchSubmit}>
     <InputWithLabel
       id="search"
       value="{searchTerm}"
@@ -113,13 +203,13 @@ const SearchForm = ({
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <button
+    <StyledButtonLarge
     type="submit"
     disabled={!searchTerm}
     >
     Submit
-    </button>
-  </form>
+    </StyledButtonLarge>
+  </StyledSearchForm>
 );
 
 //Handles syncing value of any local storage with state based on unique key id
@@ -194,8 +284,9 @@ const App = () => {
   };
 
   return(
-  <div>
-      <h1>{getTitle("My Hacker Stories")}</h1>
+  <div >
+    <StyledContainer>
+      <StyledHeadlinePrimary>{getTitle("My Hacker Stories")}</StyledHeadlinePrimary>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -203,7 +294,7 @@ const App = () => {
         onSearchSubmit={handleSearchSubmit}
       />
 
-      <hr />
+  
       { stories.isError && <p>Something went wrong...</p>}
 
       {stories.isLoading ? (
@@ -214,7 +305,7 @@ const App = () => {
           onRemoveItem={handleRemoveStory}
         />
       )}
-      
+    </StyledContainer>
     </div>
   );
   };
